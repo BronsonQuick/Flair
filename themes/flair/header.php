106e6063baad5f3daaf2145260a046fe2fa10631
Bronson Quick <bronson@sennza.com.au>
2014-02-17 15:03:27 +1000

    Add some IE functionality

--- themes/flair/header.php
+++ themes/flair/header.php
@@ -12,14 +12,25 @@
  */
 
 get_header(); ?><!doctype html>
+<!--[if IE 7]>
+<html class="ie ie7 no-js" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 no-js" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
 <html class="no-js" <?php language_attributes(); ?>>
+<!--<![endif]-->
 	<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
