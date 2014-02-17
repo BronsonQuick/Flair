dc996551834ca2e85ac8daf9dfa36537c681074f
Bronson Quick <bronson@sennza.com.au>
2014-02-17 14:06:43 +1000

    Add footer

---
+++ themes/flair/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the body tag and all content after
+ *
+ * @package Flair Theme
+ */
+?>
+<script src="bower_components/jquery/jquery.js"></script>
+<script src="bower_components/foundation/js/foundation.min.js"></script>
+<script src="js/app.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
--- themes/flair/index.php
+++ themes/flair/index.php
@@ -147,9 +147,4 @@
         </div>
       </div>
     </div>
-
-    <script src="bower_components/jquery/jquery.js"></script>
-    <script src="bower_components/foundation/js/foundation.min.js"></script>
-    <script src="js/app.js"></script>
-  </body>
-</html>
+<?php get_footer();?>
