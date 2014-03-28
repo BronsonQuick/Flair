77773a2fe64540be5a4554fc3fd3ccfce5a8e790
Bronson Quick <bronson@sennza.com.au>
2014-03-28 21:17:11 +1000

    Add 404.php template. Fixes #37

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package Flair Theme
+ */
+
+get_header(); ?>
+
+
+	<div class="row">
+
+		<!-- Main Blog Content -->
+		<div class="large-9 columns" role="content">
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Not Found', 'flair' ); ?></h1>
+			</header>
+
+			<div class="page-content">
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'flair' ); ?></p>
+
+				<?php get_search_form(); ?>
+			</div><!-- .page-content -->
+
+		</div>
+        <!-- End Main Content -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
