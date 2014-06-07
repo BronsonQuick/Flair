a0b3a3876175ef266f2348fd04f0965f7354a08c
Tarei King <tareiking@gmail.com>
2014-06-07 13:26:31 +1000

    Added grunt-contrib-compress task
    Usage: "grunt archive" to create flair-theme.zip

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,12 +24,31 @@ module.exports = function(grunt) {
 		files: 'sass/**/*.scss',
 		tasks: ['sass']
 	  }
+	},
+
+	compress: {
+		options: {
+			mode: 'zip',
+			archive: "flair-theme.zip"
+		},
+		files: {
+			expand: true,
+			src: [
+				'**',
+				'!flair-theme.zip',
+				'!bower_components/**',
+				'!node_modules/**'
+			],
+		}
 	}
-	});
+
+});
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 
 	grunt.registerTask('build', ['sass']);
 	grunt.registerTask('default', ['build','watch']);
+	grunt.registerTask('archive', ['compress']);
 }
