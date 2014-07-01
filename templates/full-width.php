6c29fd670c6b944a36882daf0791d0424e1ba10f
Bronson Quick <bronson@sennza.com.au>
2014-07-01 19:39:41 +1000

    Add a full width page template. Fixes #88

---
+++ templates/full-width.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Full Width Template
+ *
+ * The template for displaying the Sidebar on the left hand side of the page
+ *
+ * @package Flair Theme
+ */
+
+get_header(); ?>
+
+	<div class="row">
+
+		<!-- Main Blog Content -->
+		<div class="large-12 columns content" role="content">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'parts/content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) {
+						comments_template();
+					}
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+	</div>
+		<!-- End Main Content -->
+	</div>
+<?php get_footer(); ?>
