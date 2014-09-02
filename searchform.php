86f4ccbf0e9bb1dcc8e5eae2ca9f82ddab1ab1a4
Bronson Quick <bronson@sennza.com.au>
2014-09-03 01:50:50 +1000

    Remove the space after a closing parenthesis. Blame @tareiking :P

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ) ; ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="s" class="screen-reader-text"><?php _e( 'Search', 'flair' ); ?></label>
 	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'flair' ); ?>" />
 	<input type="submit" class="submit button" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'flair' ); ?>" />
