001300ad2e9c68c7d2e2e889a7b76714b5f7a7a3
Bronson Quick <bronson@sennza.com.au>
2014-12-25 15:49:57 +1000

    Fix indentation

--- search.php
+++ search.php
@@ -30,19 +30,19 @@ get_header(); ?>
 				<p><?php echo 'Your search for "' . get_search_query() . '" returned <strong>' . $search_results . '</strong> results.'; ?></p>
 
 				<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
 
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'parts/content', get_post_format() );
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'parts/content', get_post_format() );
 
-					endwhile;
-					// Previous/next post navigation.
-					flair_paging_nav();
+				endwhile;
+				// Previous/next post navigation.
+				flair_paging_nav();
 
 				else :
 					// If no content, include the "No posts found" template.
