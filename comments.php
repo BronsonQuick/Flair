5c73c77d01f5f10af65ff7ac51586c82f704d0cd
Bronson Quick <bronson@sennza.com.au>
2014-12-25 15:46:28 +1000

    Fix indentation

--- comments.php
+++ comments.php
@@ -29,8 +29,8 @@ if ( current_theme_supports( 'remove-comments' ) ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'flair' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'flair' ),
+				number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
 
@@ -57,8 +57,8 @@ if ( current_theme_supports( 'remove-comments' ) ) {
 	<?php endif; // have_comments() ?>
 
 	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'flair' ); ?></p>
 	<?php endif; ?>
