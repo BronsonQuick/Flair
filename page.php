5050cfc3acb605879d97db75097437a1c3b2d337
Bronson Quick <bronson@sennza.com.au>
2014-12-25 15:44:57 +1000

    Fix indentation

--- page.php
+++ page.php
@@ -22,10 +22,10 @@ get_header(); ?>
 				<?php get_template_part( 'parts/content', 'page' ); ?>
 
 				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) {
-						comments_template();
-					}
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) {
+					comments_template();
+				}
 				?>
 
 			<?php endwhile; // end of the loop. ?>
