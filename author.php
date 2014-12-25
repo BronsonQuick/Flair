c4950fda4db69b175c08e8399a0923f2ff5a0b72
Bronson Quick <bronson@sennza.com.au>
2014-12-25 15:24:51 +1000

    Fix indentation

--- author.php
+++ author.php
@@ -37,32 +37,32 @@ get_header(); ?>
 			</header><!-- .archive-header -->
 
 			<?php
-					/*
-					 * Since we called the_post() above, we need to rewind
-					 * the loop back to the beginning that way we can run
-					 * the loop properly, in full.
-					 */
-					rewind_posts();
+			/*
+			 * Since we called the_post() above, we need to rewind
+			 * the loop back to the beginning that way we can run
+			 * the loop properly, in full.
+			 */
+			rewind_posts();
 
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
 
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'parts/content', get_post_format() );
+				/*
+				 * Include the post format-specific template for the content. If you want to
+				 * use this in a child theme, then include a file called called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'parts/content', get_post_format() );
 
-					endwhile;
-					// Previous/next page navigation.
-					flair_paging_nav();
+			endwhile;
+			// Previous/next page navigation.
+			flair_paging_nav();
 
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'parts/content', 'none' );
+			else :
+				// If no content, include the "No posts found" template.
+				get_template_part( 'parts/content', 'none' );
 
-				endif;
+			endif;
 			?>
 
 		</div>
