ece0c33ffbb0b7ceda33eb9a396f801193c455f9
Bronson Quick <bronson@sennza.com.au>
2015-08-02 21:05:51 +1000

    Fix block comments

--- author.php
+++ author.php
@@ -19,16 +19,17 @@ get_header(); ?>
 			<header class="archive-header">
 				<h1 class="archive-title">
 					<?php
-						/*
-						 * Queue the first post, that way we know what author
-						 * we're dealing with (if that is the case).
-						 *
-						 * We reset this later so we can run the loop properly
-						 * with a call to rewind_posts().
-						 */
-						the_post();
 
-						printf( __( 'All posts by %s', 'flair' ), get_the_author() );
+					/**
+					 * Queue the first post, that way we know what author
+					 * we're dealing with (if that is the case).
+					 *
+					 * We reset this later so we can run the loop properly
+					 * with a call to rewind_posts().
+					 */
+					the_post();
+
+					printf( __( 'All posts by %s', 'flair' ), get_the_author() );
 					?>
 				</h1>
 				<?php if ( get_the_author_meta( 'description' ) ) : ?>
@@ -37,7 +38,8 @@ get_header(); ?>
 			</header><!-- .archive-header -->
 
 			<?php
-			/*
+
+			/**
 			 * Since we called the_post() above, we need to rewind
 			 * the loop back to the beginning that way we can run
 			 * the loop properly, in full.
@@ -47,7 +49,7 @@ get_header(); ?>
 			// Start the Loop.
 			while ( have_posts() ) : the_post();
 
-				/*
+				/**
 				 * Include the post format-specific template for the content. If you want to
 				 * use this in a child theme, then include a file called called content-___.php
 				 * (where ___ is the post format) and that will be used instead.
