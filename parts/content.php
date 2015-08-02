ae84a04d0a123ca180c78069da69488b5d60d043
Bronson Quick <bronson@sennza.com.au>
2015-08-02 21:49:43 +1000

    Fix up some php comments

--- parts/content.php
+++ parts/content.php
@@ -17,7 +17,7 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() || is_archive() || is_home() ) : // Only display Excerpts on Search, Archives and Blog ?>
+	<?php if ( is_search() || is_archive() || is_home() ) : // Only display Excerpts on Search, Archives and Blog. ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
@@ -34,27 +34,27 @@
 	<?php endif; ?>
 
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search. ?>
 			<?php
-			/* translators: used between list items, there is a space after the comma */
+			/* Translators: used between list items, there is a space after the comma. */
 			$categories_list = get_the_category_list( __( ', ', 'flair' ) );
 			if ( $categories_list && flair_categorized_blog() ) :
 			?>
 			<span class="cat-links">
 				<?php printf( __( 'Posted in %1$s', 'flair' ), $categories_list ); ?>
 			</span>
-			<?php endif; // End if categories ?>
+			<?php endif; ?>
 
 			<?php
-			/* translators: used between list items, there is a space after the comma */
+			/* Translators: used between list items, there is a space after the comma. */
 			$tags_list = get_the_tag_list( '', __( ', ', 'flair' ) );
 			if ( $tags_list ) :
 			?>
 			<span class="tags-links">
 				<?php printf( __( 'Tagged %1$s', 'flair' ), $tags_list ); ?>
 			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+			<?php endif; ?>
+		<?php endif; ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'flair' ), __( '1 Comment', 'flair' ), __( '% Comments', 'flair' ) ); ?></span>
