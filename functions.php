ca75c585238bd019d542a19e8ec02b84a55289d5
Bronson Quick <bronson@sennza.com.au>
2015-08-02 22:04:51 +1000

    Alter comments

--- functions.php
+++ functions.php
@@ -73,24 +73,25 @@ if ( ! function_exists( 'flair_setup' ) ) :
 
 	/**
 	 * Optionally add Foundation utilities
+	 *
+	add_theme_support( 'foundation-interchange' );
+	add_theme_support( 'foundation-top-bar' );
+	add_theme_support( 'foundation-sticky-top-bar' );
+	add_theme_support( 'foundation-magellan' );
+	add_theme_support( 'foundation-orbit' );
+	add_theme_support( 'foundation-clearing' );
+	add_theme_support( 'foundation-abide' );
+	add_theme_support( 'foundation-reveal' );
+	add_theme_support( 'foundation-alert' );
+	add_theme_support( 'foundation-tooltip' );
+	add_theme_support( 'foundation-joyride' );
+	add_theme_support( 'foundation-equalizer' );
+	add_theme_support( 'foundation-accordion' );
+	add_theme_support( 'foundation-tabs' );
+	add_theme_support( 'off-canvas' );
 	 */
-	// add_theme_support( 'foundation-interchange' );
-	// add_theme_support( 'foundation-top-bar' );
-	// add_theme_support( 'foundation-sticky-top-bar' );
-	// add_theme_support( 'foundation-magellan' );
-	// add_theme_support( 'foundation-orbit' );
-	// add_theme_support( 'foundation-clearing' );
-	// add_theme_support( 'foundation-abide' );
-	// add_theme_support( 'foundation-reveal' );
-	// add_theme_support( 'foundation-alert' );
-	// add_theme_support( 'foundation-tooltip' );
-	// add_theme_support( 'foundation-joyride' );
-	// add_theme_support( 'foundation-equalizer' );
-	// add_theme_support( 'foundation-accordion' );
-	// add_theme_support( 'foundation-tabs' );
-	// add_theme_support( 'off-canvas' );
 
-}
+	}
 endif;
 add_action( 'after_setup_theme', 'flair_setup' );
 
