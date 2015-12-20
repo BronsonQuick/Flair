fda5fb30d17c6b3c76c579042e8a4009708bd311
Bronson Quick <bronson@sennza.com.au>
2015-12-20 15:53:19 +1000

    Clean up code formatting and comments

--- category.php
+++ category.php
@@ -22,6 +22,7 @@ get_header(); ?>
 				<?php
 				// Show an optional term description.
 				$term_description = term_description();
+
 				if ( ! empty( $term_description ) ) :
 					printf( '<div class="taxonomy-description">%s</div>', $term_description );
 				endif;
@@ -32,7 +33,7 @@ get_header(); ?>
 					// Start the Loop.
 					while ( have_posts() ) : the_post();
 
-						/*
+						/**
 						 * Include the post format-specific template for the content. If you want to
 						 * use this in a child theme, then include a file called called content-___.php
 						 * (where ___ is the post format) and that will be used instead.
@@ -40,6 +41,7 @@ get_header(); ?>
 						get_template_part( 'parts/content', get_post_format() );
 
 					endwhile;
+
 					// Previous/next page navigation.
 					flair_paging_nav();
 
@@ -47,8 +49,7 @@ get_header(); ?>
 					// If no content, include the "No posts found" template.
 					get_template_part( 'parts/content', 'none' );
 
-				endif;
-			?>
+				endif; ?>
 		</div>
 		<!-- End Main Content -->
 
