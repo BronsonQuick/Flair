8ac2675351a89379bc150608e5a7a6fd62e40947
Bronson Quick <bronson@sennza.com.au>
2015-12-20 15:50:18 +1000

    Fix indentation and block comment

--- archive.php
+++ archive.php
@@ -34,22 +34,23 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
 
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'parts/content', get_post_format() );
+					/**
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'parts/content', get_post_format() );
 
-					endwhile;
-					// Previous/next page navigation.
-					flair_paging_nav();
+				endwhile;
+				// Previous/next page navigation.
+				flair_paging_nav();
 
 				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'parts/content', 'none' );
+				// If no content, include the "No posts found" template.
+				get_template_part( 'parts/content', 'none' );
 
 				endif;
 			?>
